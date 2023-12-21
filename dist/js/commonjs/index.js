6d9b4e89efa0bed4810d7f47ea6f0c2f1a15d517
pragmar <ben@pragmar.com>
2023-12-20 22:31:37 -0500

    add package.json

--- dist/js/commonjs/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-class Interrobot {
-}
-exports.default = Interrobot;
